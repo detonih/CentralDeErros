60a84fad6d46c4956b01d31c0804bb10ce1a183b
Henrique Detoni <detonihenrique@gmail.com>
2020-03-27 10:03:22 -0300

    modify config for sequelize

--- src/config/database.js
+++ src/config/database.js
@@ -1,13 +1,6 @@
 const Sequelize = require('sequelize')
 
-const database = process.env.DB_NAME
-const username = process.env.DB_USERNAME
-const password = process.env.DB_PASS
-
-const sequelize = new Sequelize(database, username, password, {
-  host: process.env.DATABASE_URL,
-  dialect: process.env.DB_DIALECT,
-  port: process.env.DB_PORT,
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialectOptions: { timezone: process.env.DB_TIMEZONE }
 })
 
