1cd14f38158dd55fe971f8d4bfef169a760dfce1
Henrique Detoni <detonihenrique@gmail.com>
2020-03-27 10:09:17 -0300

    add dialect

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,7 @@
 const Sequelize = require('sequelize')
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: process.env.DB_DIALECT,
   dialectOptions: { timezone: process.env.DB_TIMEZONE }
 })
 
