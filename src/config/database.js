26e08768acd65387ecdfb619d4482890698b384a
Henrique Detoni <detonihenrique@gmail.com>
2020-03-27 09:38:07 -0300

    add db port to sequelize

--- src/config/database.js
+++ src/config/database.js
@@ -7,6 +7,7 @@ const password = process.env.DB_PASS
 const sequelize = new Sequelize(database, username, password, {
   host: process.env.DATABASE_URL,
   dialect: process.env.DB_DIALECT,
+  port: process.env.DB_PORT,
   dialectOptions: { timezone: process.env.DB_TIMEZONE }
 })
 
